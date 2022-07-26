74d364c82ab376d08cbe6513b75d624d78410e8e
앵가리 <>
2022-07-26 22:53:57 +0900

    feat: 커스텀 user admin 추가

--- user/admin.py
+++ user/admin.py
@@ -1,5 +1,36 @@
 from django.contrib import admin
 from user.models import User
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 # Register your models here.
-admin.site.register(User)
+class UserAdmin(BaseUserAdmin):
+    list_display = ('id', 'username', 'fullname', 'email',)
+    list_display_links = ('username', )
+    list_filter = ('username', )
+    search_fields = ('username', 'email', )
+
+    fieldsets = (
+        ("info", {
+            'fields': ('username', 'fullname', 'email', 'password')
+        
+        }),
+        ('date', {
+            'fields': ('created_at', 'updated_at',)
+        }),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide', ),
+            'fields': ('username', 'fullname', 'email', 'password1', 'password2' )
+        }),
+    )
+
+    filter_horizontal = []
+
+    def get_readonly_fields(self, request, obj=None):
+        if obj:
+            return('username', 'created_at', 'updated_at', )
+        else:
+            return('created_at', 'updated_at', )
+
+admin.site.register(User, UserAdmin)
