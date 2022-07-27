23c783abd238b5f4d901607b2dcdf77d350e1d82
앵가리 <>
2022-07-27 16:14:58 +0900

    feat: custom jwt 추가

--- user/urls.py
+++ user/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from user import views
 from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
+    # TokenObtainPairView,
     TokenRefreshView,
 )
 
 urlpatterns = [
     path('', views.UserView.as_view()),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/', views.login.as_view(), name='token_obtain_pair'),
+    # path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- user/views.py
+++ user/views.py
@@ -4,6 +4,9 @@ from rest_framework.response import Response
 
 from user.serializers import UserSerializer
 
+from rest_framework_simplejwt.views import TokenObtainPairView
+from user.serializers_jwt import TokenObtainPairSerializer
+
 # Create your views here.
 class UserView(APIView):
 
@@ -18,3 +21,9 @@ class UserView(APIView):
             serializer.save()
             return Response({"massege": "회원가입이 완료되었습니다", "response": serializer.data}, status=status.HTTP_200_OK)
         return Response({"massege": "회원가입 오류", "response": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+
+# 로그인
+class login(TokenObtainPairView):
+    serializer_class = TokenObtainPairSerializer
+
+
